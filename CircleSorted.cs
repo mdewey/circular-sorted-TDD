5408ddd1c7d67ddde5489bf3d1eb64888f522a32
Mark Dewey <mark.t.dewey@gmail.com>
2019-09-18 17:18:17 -0400

    first!

---
+++ CircleSorted.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace testing
+{
+  public static class CircleSorted
+  {
+    public static bool IsCircleSorted(int[] arr)
+    {
+      var things = arr
+        .Select((current, index) => new { current, next = arr[(index + 1) % arr.Count()] });
+      var count = things.Count(a => a.current > a.next);
+      return count <= 1;
+
+    }
+  }
+}
---
+++ CircleTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Xunit;
+
+namespace testing
+{
+  public class CircleTests
+  {
+    [Theory]
+    [InlineData(new int[] { 2, 3, 4, 5, 0, 1 })]
+    [InlineData(new int[] { 4, 5, 6, 9, 1 })]
+    [InlineData(new int[] { 10, 11, 6, 7, 9 })]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new int[] { 5, 7, 43, 987, -9, 0 })]
+    [InlineData(new int[] { 1, 2, 3, 4, 1 })]
+    public void TrueCases(int[] arr)
+    {
+      Assert.True(CircleSorted.IsCircleSorted(arr));
+    }
+    [Theory]
+    [InlineData(new int[] { 4, 1, 2, 5 })]
+    [InlineData(new int[] { 8, 7, 6, 5, 4, 3 })]
+    [InlineData(new int[] { 6, 7, 4, 8 })]
+    [InlineData(new int[] { 7, 6, 5, 4, 3, 2, 1 })]
+    public void FalseCases(int[] arr)
+    {
+      Assert.False(CircleSorted.IsCircleSorted(arr));
+    }
+
+
+  }
+}
